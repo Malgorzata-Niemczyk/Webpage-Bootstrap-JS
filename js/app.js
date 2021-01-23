b06b14da41ba05ef0bf12457c64159fe71c83415
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-01-24 00:56:58 +0100

    use the function from jquery that allows to close the navbar when clicking anyway on the screen

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 // to firstly load the content and then execute the scripts
 document.addEventListener('DOMContentLoaded', () => {
- const navBarElement = document.querySelector('.navbar');
+  const navBarElement = document.querySelector('.navbar');
 
- function addShadow() {
-    if (window.scrollY >= 300) {
-        navBarElement.classList.add('shadow-bg');
-    } else {
-        navBarElement.classList.remove('shadow-bg');
-    }
-  };
-  
-  window.addEventListener('scroll', addShadow);
+  function addShadow() {
+      if (window.scrollY >= 300) {
+          navBarElement.classList.add('shadow-bg');
+      } else {
+          navBarElement.classList.remove('shadow-bg');
+      }
+    };
+    
+    window.addEventListener('scroll', addShadow);
+
+    $(document).ready(function () {
+      $(document).click(function (event) {
+          var clickover = $(event.target);
+          var _opened = $(".navbar-collapse").hasClass("show");
+          if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+              $(".navbar-toggler").click();
+          }
+      });
+  });
 });
