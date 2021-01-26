ab858c98e04078cbd9544e22db10558bb9c46775
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-01-26 02:12:56 +0100

    add modifications to the app.js to add the function for slick

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navBarElement = document.querySelector('.navbar');
 
+  // function that adds the shadow to the navigation when scrolling down
   function addShadow() {
       if (window.scrollY >= 300) {
           navBarElement.classList.add('shadow-bg');
@@ -12,13 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
     
     window.addEventListener('scroll', addShadow);
 
-    $(document).ready(function () {
-      $(document).click(function (event) {
-          var clickover = $(event.target);
-          var _opened = $(".navbar-collapse").hasClass("show");
-          if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+    // script fixing navigation (the navigation closes upon clicking any place of the mobile screen)
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
               $(".navbar-toggler").click();
-          }
-      });
-  });
+        }
+    });
+
+    // slickJS
+    $('.your-class').slick({
+    //   setting - name: setting-value
+    });
+
+
 });
